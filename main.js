c6224f4d63174a985f43ac75327627a9084158f3
Wendel Bueno <wendelbueno99@gmail.com>
2024-08-07 15:59:44 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-    console.log(document.querySelector('header button'))
-    console.log($('#botao-cancelar'))
+    $('header button').click(function() {
+        alert("Expandir formulário")
+    })
+
+    $('form').on('submit', function(e) {
+        console.log("submit");
+        e.preventDefault();
+    })
 })
+
